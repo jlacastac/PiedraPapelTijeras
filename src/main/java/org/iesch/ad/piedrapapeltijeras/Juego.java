87f09a720560eff0fc634d6b38ab54ab4ad7f3aa
jlacastac <jlacastac@iesch.org>
2022-10-04 17:23:21 +0200

    0.1 Estructura del proyecto y librerias incluidas.

---
+++ src/main/java/org/iesch/ad/piedrapapeltijeras/Juego.java
@@ -0,0 +1,10 @@
+package org.iesch.ad.piedrapapeltijeras;
+
+
+public class Juego {
+
+    public static final int PIEDRA = 0;
+    public static final int PAPEL = 1;
+    public static final int TIJERA = 2;
+
+}
